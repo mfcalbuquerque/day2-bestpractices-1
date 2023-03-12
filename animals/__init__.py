25e741af4215743edf8274f785cfb296b2804562
mfcalbuquerque <mfcaf3.14@gmail.com>
2023-03-12 19:13:31 +0100

    Exercises day2

--- animals/__init__.py
+++ animals/__init__.py
@@ -1,3 +1,6 @@
-from .mammals import Mammals
-from .birds import Birds
-from .fish import Fish
+#from .mammals import Mammals
+#from .birds import Birds
+#from .fish import Fish
+
+from .harmless import birds
+from .dangerous import fish
--- test_animals.py
+++ test_animals.py
@@ -1,15 +1,17 @@
 # Import classes from your brand new package
-from animals import Mammals
-from animals import Birds
-from animals import Fish
+#from animals import Mammals
+#from animals import Birds
+#from animals import Fish
+
+import animals
 
 # Create an object of Mammals class & call a method of it
-myMammal = Mammals()
+myMammal = animals.Mammals()
 myMammal.printMembers()
 
 # Create an object of Birds class & call a method of it
-myBird = Birds()
+myBird = animals.Birds()
 myBird.printMembers()
 
-myFish = Fish()
+myFish = animals.Fish()
 myFish.printMembers()
